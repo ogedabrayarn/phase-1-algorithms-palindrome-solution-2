fda965cf67bc28158ede6952ee9d47f6e7f397f3
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-27 15:45:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  var reg = /[\W_]/g; //the slash signifies a start of regular expression. 
+  //backslash & W to match all alphanumeric values, g  for global
+  var smallStr = word.toLowerCase().replace(reg, '');
 
+  var reversed = smallStr.split('').reverse().join('');
+  
+  if(reversed === smallStr) return true;
+
+    return false;
+}
+isPalindrome('racecar');
 /* 
   Add your pseudocode here
 */
@@ -13,13 +22,21 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('a'));
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting: false');
+  console.log('ab', isPalindrome('ab'));
 }
 
 module.exports = isPalindrome;
